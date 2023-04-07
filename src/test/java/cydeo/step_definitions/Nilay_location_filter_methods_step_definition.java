167bec6c37012e39665d49f8ca25891e784f7be0
soninilay <nilay.soni@hotmail.co.uk>
2023-04-07 21:40:43 +0100

    my commit-nilay.

---
+++ src/test/java/cydeo/step_definitions/Nilay_location_filter_methods_step_definition.java
@@ -0,0 +1,66 @@
+package cydeo.step_definitions;
+
+import cydeo.pages.LoginPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Nilay_location_filter_methods_step_definition {
+    public LoginPage loginPage = new LoginPage();
+
+    @When("user lands on Quick Launchpad")
+    public void user_lands_on_quick_launchpad() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user selects Vehicle under Fleet menu")
+    public void user_selects_vehicle_under_fleet_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user lands on Cars page")
+    public void user_lands_on_cars_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user clicks on filters icon")
+    public void user_clicks_on_filters_icon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user clicks on manage filters and selects location checkbox")
+    public void user_clicks_on_manage_filters_and_selects_location_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user clicks on location button to see various methods")
+    public void user_clicks_on_location_button_to_see_various_methods() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user validate {string} method is displayed")
+    public void user_validate_method_is_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Then("user selects {string} method")
+    public void user_selects_method(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user enters keyword in the search box and click update button")
+    public void user_enters_keyword_in_the_search_box_and_click_update_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user verifies the results")
+    public void user_verifies_the_results() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
