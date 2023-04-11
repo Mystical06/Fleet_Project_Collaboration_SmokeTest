7170b2ed049bf01684b1b47021967877fa7f87cb
Mustafa Aydin <97643186+Mystical06@users.noreply.github.com>
2023-04-11 01:15:20 +0100

    Ambreen driver change

--- src/test/java/cydeo/step_definitions/VehicleTableViewStep_DefinitionAmbreen.java
+++ src/test/java/cydeo/step_definitions/VehicleTableViewStep_DefinitionAmbreen.java
@@ -9,13 +9,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class VehicleTableViewStep_DefinitionAmbreen {
 
@@ -35,7 +28,7 @@ public class VehicleTableViewStep_DefinitionAmbreen {
         String password = ConfigurationReader.getProperty("all.pw");
 
         BasePage basePage = new BasePage();
-        basePage.DriverLogin();
+        basePage.driverLogin();
 
     }
 
